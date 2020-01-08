6de5ec538f6dc35d64874244bb1e8a2dd19e7d9a
Dan Martin <daniel.martin@ellucian.com>
2020-01-08 10:11:03 -0500

    first commit

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import Auxilliary from '../../../hoc/Auxilliary';
+
+import classes from './Person.css';
+
+class Person extends Component {
+  render() {
+    console.log('[Person.js] rendering...');
+    return 
+    <Auxilliary>
+        <p key='i1' onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old!
+        </p>,
+        <p key='i2' >{this.props.children}</p>,
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+          key='i3' 
+        />
+    </Auxilliary>
+        
+    
+  }
+}
+
+export default Person;
